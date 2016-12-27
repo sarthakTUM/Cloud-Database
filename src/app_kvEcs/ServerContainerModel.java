f50437cfb7dcc0aa3a3593bb6f110faa8f45b138
LAPTOP-26KTA4VH\Anant <anant@wom.by>
2016-12-27 18:14:24 +0100

    build server and servermodel functions.

--- src/app_kvEcs/ServerContainerModel.java
+++ src/app_kvEcs/ServerContainerModel.java
@@ -3,25 +3,64 @@ package app_kvEcs;
 
 
 public class ServerContainerModel {
-	private List<ServerModel> Serverlist = new ArrayList<ServerModel>();
+	private List<ServerModel> ServerList = new ArrayList<ServerModel>();
 	
 	String Delimiter;//the delimiter of the metadata
 	
-	private boolean add()
+	private boolean add(ServerModel NewNode)
 	{
-		list.add(e)
+		this.ServerList.add(NewNode);
 	}// appends a node to the serverList array
-	private boolean remove()
+	private boolean remove(int Index)
 	{
-		
+		this.ServerList.remove(Index);
 	}//remove a node at an arbitary position, 
 	private String stringify()
 	{
-		
+	for (int ctr=0; ctr<this.ServerList.size(); ctr++)	
+	{
+		StringBuilder builder = new StringBuilder();
+		ServerModel Temp= ServerList.get(ctr);
+		if(Temp.getName()!=null)
+		{
+		builder.append(Temp.getName());
+		builder.append(",");
+		}
+		if(Temp.getIP()!=null)
+		{
+		builder.append(Temp.getIP());
+		builder.append(",");
+		}
+		if(Temp.getPort()>0)
+		{
+		builder.append(Temp.getPort());
+		builder.append(",");
+		}
+		if(Temp.getStartIndex()>0)
+		{
+		builder.append(Temp.getStartIndex());
+		builder.append(",");
+		}
+		if(Temp.getEndIndex()>0)
+		{
+		builder.append(Temp.getEndIndex());
+		builder.append(",");
+		}
+         builder.append("\n");
+         return builder.toString();
+	}
 	}// converts the serverList object into a metadata format for passing to server, delimited with the specified delimiter
 	private boolean sortHash()
 	{
-		
+		Collections.sort(this.ServerList, new Comparator<ServerModel>() {
+	        @Override
+	        public int compare(ServerModel Server2, ServerModel Server1)
+	        {
+
+Integer i = new Integer(Server1.getHashValue()); 
+	            return  i.compareTo(Server2.getHashValue();
+	        }
+	    });
 	}//sorts the serverList as per the hash value
 
 	private boolean isSorted()
@@ -29,9 +68,12 @@ public class ServerContainerModel {
 		
 	}//returns true if the serverList is already sorted in ascending order
 
-	private String prepareMetaData()
+	private void prepareMetaData()
 	{
-
+		for (int ctr=0; ctr<this.ServerList.size(); ctr++)	
+		{
+		 
+		}
     }//populates beginning index and end index, if isSORTED() function returns true;
 
 	//getters and setters for serverList
--- src/app_kvEcs/ServerModel.java
+++ src/app_kvEcs/ServerModel.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package app_kvEcs;
 
 /**
@@ -13,6 +10,98 @@ private String IP;
 private int Port;
 private int HashValue;
 private int StartIndex, EndIndex;
+
+public ServerModel(String name, String iP, int port) {
+	
+	this.Name = name;
+	this.IP = iP;
+	this.Port = port;
+
+}
+public ServerModel(String name, String iP, int port, int hashValue, int startIndex, int endIndex) {
+	
+	this.Name = name;
+	this.IP = iP;
+	this.Port = port;
+	this.HashValue = hashValue;
+	this.StartIndex = startIndex;
+	this.EndIndex = endIndex;
+}
+
+
+/**
+ * @return the name
+ */
+public String getName() {
+	return Name;
+}
+/**
+ * @param name the name to set
+ */
+public void setName(String name) {
+	Name = name;
+}
+/**
+ * @return the iP
+ */
+public String getIP() {
+	return IP;
+}
+/**
+ * @param iP the iP to set
+ */
+public void setIP(String iP) {
+	IP = iP;
+}
+/**
+ * @return the port
+ */
+public int getPort() {
+	return Port;
+}
+/**
+ * @param port the port to set
+ */
+public void setPort(int port) {
+	Port = port;
+}
+/**
+ * @return the hashValue
+ */
+public int getHashValue() {
+	return HashValue;
+}
+/**
+ * @param hashValue the hashValue to set
+ */
+public void setHashValue(int hashValue) {
+	HashValue = hashValue;
+}
+/**
+ * @return the startIndex
+ */
+public int getStartIndex() {
+	return StartIndex;
+}
+/**
+ * @param startIndex the startIndex to set
+ */
+public void setStartIndex(int startIndex) {
+	StartIndex = startIndex;
+}
+/**
+ * @return the endIndex
+ */
+public int getEndIndex() {
+	return EndIndex;
+}
+/**
+ * @param endIndex the endIndex to set
+ */
+public void setEndIndex(int endIndex) {
+	EndIndex = endIndex;
+}
+
 
 }
 
