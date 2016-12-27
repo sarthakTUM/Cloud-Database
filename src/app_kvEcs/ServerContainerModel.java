4ecd4b697bc8266797c12b2456a45c6934337185
LAPTOP-26KTA4VH\Anant <anant@wom.by>
2016-12-27 16:12:11 +0100

    create basic serverModel and ServercontainerModel classes

---
+++ src/app_kvEcs/ServerContainerModel.java
@@ -0,0 +1,38 @@
+import java.util.*;
+package app_kvEcs;
+
+
+public class ServerContainerModel {
+	private List<ServerModel> Serverlist = new ArrayList<ServerModel>();
+	
+	String Delimiter;//the delimiter of the metadata
+	
+	private boolean add()
+	{
+		list.add(e)
+	}// appends a node to the serverList array
+	private boolean remove()
+	{
+		
+	}//remove a node at an arbitary position, 
+	private String stringify()
+	{
+		
+	}// converts the serverList object into a metadata format for passing to server, delimited with the specified delimiter
+	private boolean sortHash()
+	{
+		
+	}//sorts the serverList as per the hash value
+
+	private boolean isSorted()
+	{
+		
+	}//returns true if the serverList is already sorted in ascending order
+
+	private String prepareMetaData()
+	{
+
+    }//populates beginning index and end index, if isSORTED() function returns true;
+
+	//getters and setters for serverList
+}
---
+++ src/app_kvEcs/ServerModel.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package app_kvEcs;
+
+/**
+ * @author Anant
+ *
+ */
+public class ServerModel {
+private String Name;
+private String IP;
+private int Port;
+private int HashValue;
+private int StartIndex, EndIndex;
+
+}
+
